46ee7518a10b7e1cbf35f651b418e3a8914563f5
Kathy * <Kathy@KathyLambert.me>
2019-05-21 11:33:52 -0400

    Added a form with input and button to the StorePicker component.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,8 +3,14 @@ import React from 'react'
 class StorePicker extends React.Component {
   render(){
     return (
-      <p>Sup'</p>
-    )
+      <React.Fragment>
+      <form action="" className="store-selector">
+        <h3>Please Enter A Store</h3>
+        <input type="text" required placeholder='StoreName'/>
+        <button type="submit"> Visit Store -> </button>
+      </form>
+      </React.Fragment>
+      )
   }
 }
 
